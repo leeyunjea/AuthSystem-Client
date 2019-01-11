fa24fae3105296606cf30f75b6ab68a47c686b9a
leeyunjea <yunjea0312@naver.com>
2019-01-12 01:39:52 +0900

    Update AdminPage plus table

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="hello">
+  <div id="AdminPage">
     <h1>{{ msg }}</h1>
      <h2>Admin Page</h2>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>
-      <br>
-      {{user}}
-<br>
+<p>
+    <ul>
     
-    </p>
+<table
+    class="type06"
+>
+<tr class="even">
+    <th>Id</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Update</th>
+    <th>Delete</th>
+</tr>
+    <tr v-for="user in users">
+      <td> {{ user.userId }} </td> 
+      <td>{{user.name}} </td>
+      <td>{{user.email}} </td>
+      <td><button @click="update">Update</button></td>
+      <td><button @click="filter">Delete</button></td>
+
+    </tr>
+    </table>
+
+    </ul>
+</p>
+
+<!-- <vue-virtual-table
+    :config="tableConfig"
+    :data="tableData"
+    :height="800"
+    :itemHeight="55"
+    :minWidth="1000"
+    :selectable="true"
+    :enableExport="true"
+    v-on:changeSelection="handleSelectionChange"
+  >
+
+  <template slot-scope="scope" slot="actionCommon">
+      <button @click="edit(scope.index, scope.row)">Edit</button>
+      <button @click="del(scope.index, scope.row)">Delete</button>
+    </template>
+
+  </vue-virtual-table> -->
+
+    
     
    
   </div>
@@ -20,16 +56,39 @@
 
 <script>
 import axios from "axios";
+import VueVirtualTable from "vue-virtual-table";
 
 export default {
+  el: "AdminPage",
   name: "AdminPage",
+
+  components: {
+    VueVirtualTable
+  },
+
   props: {
     msg: String,
-    user: Array
+    users: Array
   },
 
   data: {
-    user: ''
+    // users: '',
+    users: [
+      {
+        userId: "",
+        name: "",
+        email: ""
+      }
+    ],
+    tableConfig: [
+      { prop: "user", name: "User Name", searchable: true },
+      { prop: "age", name: "Age", numberFilter: true }
+    ],
+    tableData: [
+      { user: "a1", age: 20 },
+      { user: "a2", age: 21 },
+      { user: "a3", age: 23 }
+    ]
   },
 
   created() {
@@ -39,13 +98,25 @@ export default {
       .post("http://localhost:8082/api/admin/listUsers")
       .then(response => {
         debugger;
-        this.user = response.data;
+        this.users = response.data;
       })
       .catch(e => {
         debugger;
         // location.href = "./login";
         this.errors.push(e);
       });
+  },
+
+  methods: {
+    handleSelectionChange(rows) {
+      console.log(rows);
+    },
+    edit(index, row) {
+      console.log(index);
+    },
+    del(index, row) {
+      console.log(index);
+    }
   }
 };
 </script>
@@ -64,5 +135,51 @@ li {
 }
 a {
   color: #42b983;
+}
+div {
+    align-content: center;
+}
+table {
+    /* padding: 0;
+    height : 500;
+    border: 1;
+    align-content: center; */
+    margin-left: 36%;
+    itemHeight : 40;
+    
+    minWidth : '1000';
+    selectable:'true';
+    enableExport:'true';
+}
+td {
+  /* display: table-cell;
+  border: 1px solid #333;
+  vertical-align: middle;
+  height:50px; */
+}
+th {
+    /* background-color: coral; */
+}
+table.type06 {
+    border-collapse: collapse;
+    text-align: left;
+    line-height: 1.5;
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    margin: 20px 10px;
+}
+table.type06 th {
+    width: 150px;
+    padding: 10px;
+    font-weight: bold;
+    vertical-align: top;
+}
+table.type06 td {
+    width: 350px;
+    padding: 10px;
+    vertical-align: top;
+}
+table.type06 .even {
+    background: #efefef;
 }
 </style>
